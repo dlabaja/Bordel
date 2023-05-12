caa8405ba982e67c7a69b2d315ab0f835f438f7f
Ten dlabaja <72014646+dlabaja@users.noreply.github.com>
2023-05-12 15:32:45 +0200

    Add files via upload

---
+++ PostfixCalculator.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostfixCalculator
+{
+	public class PostfixCalculator
+	{
+		Stack<double> buffer = new Stack<double>();
+		Dictionary<string, Func<double>> operations;
+		private bool _mathError;
+
+		public void InitDict() => operations = new Dictionary<string, Func<double>>()
+		{
+			{ "+", () => buffer.Pop() + buffer.Pop()},
+			{ "-", () => buffer.Pop() - buffer.Pop()},
+			{ "*", () => buffer.Pop() * buffer.Pop()},
+			{ "/", () => {
+				var num1 = buffer.Pop();
+				var num2 = buffer.Pop();
+				if (num2 == 0) _mathError = true;
+				return num1 / num2; }
+				},
+			{ "%", () => buffer.Pop() % buffer.Pop()},
+			{ "sin", () => Math.Sin(buffer.Pop())},
+			{ "cos", () => Math.Cos(buffer.Pop())},
+			{ "tg", () => Math.Sin(buffer.Pop())},
+			{ "cotg", () => {
+				var num1 = buffer.Pop();
+				var num2 = buffer.Pop();
+				if (num2 == 0) _mathError = true;
+				return Math.Cos(num1) / Math.Sin(num2); } },
+			{ "pow", () => Math.Pow(buffer.Pop(), 2)},
+			{ "sqrt", () =>{
+				var num = buffer.Pop();
+				if (num < 0) _mathError = true;
+				return Math.Sqrt(num); } },
+			{ "log", () => {
+				var num = buffer.Pop();
+				if (num <= 0) _mathError = true;
+				return Math.Log10(num); }},
+			{ "ln", () => {
+				var num = buffer.Pop();
+				if (num <= 0) _mathError = true;
+				return Math.Log(num); }},
+			{ "abs", () => Math.Abs(buffer.Pop())},
+			{ "π", () => Math.PI},
+			{ "e", () => Math.E}
+		};
+
+		public double Calculate(dynamic[] commands)
+		{
+			InitDict();
+			for (int i = 0; i < commands.Length; i++)
+			{
+				if (operations.ContainsKey(Convert.ToString(commands[i])))
+					buffer.Push(operations[Convert.ToString(commands[i])]());
+				else
+					buffer.Push(Convert.ToDouble(commands[i]));
+			}
+			return buffer.Pop();
+		}
+
+		//infix musí obsahovat mezery
+		public dynamic[] InfixToPostfix(string infix)
+		{
+			InitDict();
+			var postfix = new List<dynamic>();
+			var stack = new Stack<string>();
+
+			infix += " )";
+			
+
+			foreach (var item in infix.Split())
+			{
+				if (item == "(")
+					stack.Push("(");
+				else if (double.TryParse(item, out var d))
+					postfix.Add(d);
+				else if (item == ")")
+				{
+					while (stack.Count > 0 && stack.Peek() != "(")
+						postfix.Add(stack.Pop());
+					if (stack.Count > 0) stack.Pop();
+				}
+				else
+				{
+					while (stack.Count > 0 && stack.Peek() != "(" && GetPrecedence(stack.Peek()) >= GetPrecedence(item))
+						postfix.Add(stack.Pop());
+					stack.Push(item);
+				}
+			}
+			while (stack.Count > 0)
+				postfix.Add(stack.Pop());
+
+			return postfix.ToArray();
+		}
+
+		private static int GetPrecedence(string op)
+		{
+			switch (op)
+			{
+				case "+":
+				case "-":
+					return 1;
+				case "*":
+				case "/":
+				case "%":
+					return 2;
+				default:
+					return 0;
+			}
+		}
+	}
+}

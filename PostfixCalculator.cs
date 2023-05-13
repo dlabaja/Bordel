ff0c2c251b0f2fc18aab3c0f0f4ee322b1807c48
Ten dlabaja <72014646+dlabaja@users.noreply.github.com>
2023-05-13 20:34:01 +0200

    Update PostfixCalculator.cs

--- PostfixCalculator.cs
+++ PostfixCalculator.cs
@@ -3,132 +3,174 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace PostfixCalculator
 {
-	public class PostfixCalculator
-	{
-		Stack<double> buffer = new Stack<double>();
-		Dictionary<string, Func<double>> operations;
+    public class PostfixCalculator
+    {
+        private readonly Stack<double> _buffer = new Stack<double>();
+        private Dictionary<string, Func<double>> _operations;
+        private string _exceptionMessage = "Invalid Syntax";
 
-		public void InitDict() => operations = new Dictionary<string, Func<double>>()
-		{
-			{ "+", () => buffer.Pop() + buffer.Pop()},
-			{ "-", () => {
-				var num2 = buffer.Pop();
-				var num1 = buffer.Pop();
-				return num1 - num2; } },
-			{ "*", () => buffer.Pop() * buffer.Pop()},
-			{ "/", () => {
-				var num2 = buffer.Pop();
-				var num1 = buffer.Pop();
-				if (num2 == 0) throw new Exception("Math error");
-				return num1 / num2; }
-				},
-			{ "%", () => buffer.Pop() % buffer.Pop()},
-			{ "sin", () => Math.Sin(buffer.Pop())},
-			{ "cos", () => Math.Cos(buffer.Pop())},
-			{ "tg", () => Math.Sin(buffer.Pop())},
-			{ "cotg", () => {
-				var num2 = buffer.Pop();
-				var num1 = buffer.Pop();
-				if (num2 == 0) throw new Exception("Math error");
-				return Math.Cos(num1) / Math.Sin(num2); } },
-			{ "pow", () => Math.Pow(buffer.Pop(), 2)},
-			{ "sqrt", () =>{
-				var num = buffer.Pop();
-				if (num < 0) throw new Exception("Math error");
-				return Math.Sqrt(num); } },
-			{ "log", () => {
-				var num = buffer.Pop();
-				if (num <= 0) throw new Exception("Math error");
-				return Math.Log10(num); }},
-			{ "ln", () => {
-				var num = buffer.Pop();
-				if (num <= 0) throw new Exception("Math error");
-				return Math.Log(num); }},
-			{ "abs", () => Math.Abs(buffer.Pop())},
-			{ "π", () => Math.PI},
-			{ "e", () => Math.E}
-		};
+        private void InitDict() => _operations = new Dictionary<string, Func<double>>
+        {
+            {"+", () => _buffer.Pop() + _buffer.Pop()},
+            {
+                "-", () =>
+                {
+                    var num2 = _buffer.Pop();
+                    var num1 = _buffer.Pop();
+                    return num1 - num2;
+                }
+            },
+            {"*", () => _buffer.Pop() * _buffer.Pop()},
+            {
+                "/", () =>
+                {
+                    var num2 = _buffer.Pop();
+                    var num1 = _buffer.Pop();
+                    if (num2 == 0) ThrowException("Cannot divide by zero");
+                    return num1 / num2;
+                }
+            },
+            {
+                "%", () =>
+                {
+                    var num2 = _buffer.Pop();
+                    var num1 = _buffer.Pop();
+                    return num1 % num2;
+                }
+            },
+            {"sin", () => Math.Sin(_buffer.Pop())},
+            {"cos", () => Math.Cos(_buffer.Pop())},
+            {"tg", () => Math.Sin(_buffer.Pop())},
+            {
+                "cotg", () =>
+                {
+                    var num = _buffer.Pop();
+                    if (num == 0) ThrowException("Cotg() cannot be zero");
+                    return Math.Cos(num) / Math.Sin(num);
+                }
+            },
+            {"pow", () => Math.Pow(_buffer.Pop(), 2)},
+            {
+                "sqrt", () =>
+                {
+                    var num = _buffer.Pop();
+                    if (num < 0) ThrowException("Sqrt() cannot be < 0");
+                    return Math.Sqrt(num);
+                }
+            },
+            {
+                "log", () =>
+                {
+                    var num = _buffer.Pop();
+                    if (num <= 0) ThrowException("Log() cannot be ≤ 0");
+                    return Math.Log10(num);
+                }
+            },
+            {
+                "ln", () =>
+                {
+                    var num = _buffer.Pop();
+                    if (num <= 0) ThrowException("Ln() cannot be ≤ 0");
+                    return Math.Log(num);
+                }
+            },
+            {"abs", () => Math.Abs(_buffer.Pop())},
+            {"π", () => Math.PI},
+            {"e", () => Math.E}
+        };
 
-		public object Calculate(dynamic[] commands)
-		{
-			try
-			{
-				InitDict();
-				foreach (var item in commands)				
-				{
-					if (operations.ContainsKey(Convert.ToString(item)))
-						buffer.Push(operations[Convert.ToString(item)]());
-					else
-						try { 
-						buffer.Push(Convert.ToDouble(item));
-						}
-						catch
-						{
-							throw new Exception("Invalid Syntax");
-						}
-				}
-				if (buffer.Count > 1)
-					throw new Exception("Invalid Syntax");
-			}
-			catch (Exception e)
-			{
-				return e.Message;
-			}
-			return buffer.Pop();
-		}
+        public object CalculatePostfix(IEnumerable<dynamic> commands)
+        {
+            try
+            {
+                InitDict();
+                foreach (var item in commands)
+                    _buffer.Push(_operations.ContainsKey(Convert.ToString(item))
+                        ? (double) _operations[Convert.ToString(item)]()
+                        : (double) Convert.ToDouble(item));
 
-		public dynamic[] InfixToPostfix(string infix)
-		{
-			InitDict();
-			var postfix = new List<dynamic>();
-			var stack = new Stack<string>();
+                if (_buffer.Count > 1) throw new Exception(); // more than one number in result
+            }
+            catch
+            {
+                return _exceptionMessage;
+            }
 
-			infix += " )";
-			
+            return _buffer.Pop();
+        }
 
-			foreach (var item in infix.Split())
-			{
-				if (item == "(")
-					stack.Push("(");
-				else if (double.TryParse(item, out var d))
-					postfix.Add(d);
-				else if (item == ")")
-				{
-					while (stack.Count > 0 && stack.Peek() != "(")
-						postfix.Add(stack.Pop());
-					if (stack.Count > 0) stack.Pop();
-				}
-				else
-				{
-					while (stack.Count > 0 && stack.Peek() != "(" && GetPrecedence(stack.Peek()) >= GetPrecedence(item))
-						postfix.Add(stack.Pop());
-					stack.Push(item);
-				}
-			}
-			while (stack.Count > 0)
-				postfix.Add(stack.Pop());
+        private void ThrowException(string msg) //custom exceptions
+        {
+            _exceptionMessage = msg;
+            throw new Exception();
+        }
 
-			return postfix.ToArray();
-		}
+        public IEnumerable<dynamic> InfixToPostfix(StringBuilder infix)
+        {
+            InitDict();
+            var postfix = new List<dynamic>();
+            var stack = new Stack<string>();
+            
+            //replace constants
+            infix = infix.Replace("π", Math.PI.ToString(CultureInfo.InvariantCulture));
+            infix = infix.Replace("e", Math.E.ToString(CultureInfo.InvariantCulture));
 
-		private static int GetPrecedence(string op)
-		{
-			switch (op)
-			{
-				case "+":
-				case "-":
-					return 1;
-				case "*":
-				case "/":
-				case "%":
-					return 2;
-				default:
-					return 0;
-			}
-		}
-	}
+            // fix for negative numbers -> adds 0 if '-' is after '(' or at the start of infix 
+            if (infix[0] == '-')
+                infix.Insert(0, '0');
+            for (var i = 1; i < infix.Length; i++)
+            {
+                if (infix[i] == '-' && infix[i - 1] == '(')
+                    infix.Insert(i, '0');
+            }
+
+            foreach (Match item in Regex.Matches(infix.ToString(), @"[+\%\-\*\/()]|\d*\.?\d+|[a-z]+"))
+            {
+                if (item.Value == "(") // start of stack region
+                    stack.Push("(");
+                else if (double.TryParse(item.Value, NumberStyles.Any, CultureInfo.InvariantCulture,
+                             out var d)) // number
+                    postfix.Add(d);
+                else if (item.Value == ")") // pushes all of stack to postfix until '(' is met
+                {
+                    while (stack.Count > 0 && stack.Peek() != "(")
+                        postfix.Add(stack.Pop());
+                    if (stack.Count > 0) stack.Pop();
+                }
+                else // pushes operator/function to stack by precedence
+                {
+                    while (stack.Count > 0 && stack.Peek() != "(" &&
+                           GetPrecedence(stack.Peek()) >= GetPrecedence(item.ToString()))
+                        postfix.Add(stack.Pop());
+                    stack.Push(item.Value);
+                }
+            }
+
+            while (stack.Count > 0) // empty stack
+                postfix.Add(stack.Pop());
+
+            return postfix.ToArray();
+        }
+
+        private static int GetPrecedence(string op)
+        {
+            switch (op)
+            {
+                case "+":
+                case "-":
+                    return 1;
+                case "*":
+                case "/":
+                case "%":
+                    return 2;
+                default:
+                    return 0;
+            }
+        }
+    }
 }
